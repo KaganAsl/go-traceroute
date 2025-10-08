0c9c80ba66a8dbf69626fd09c85170ab568b70c7
Kagan <kagan.aslancan@gmail.com>
2025-10-08 11:35:47 +0300

    sending multiiple packets with multiple ttl values

--- capture/icmp_capture_windows.go
+++ capture/icmp_capture_windows.go
@@ -2,4 +2,16 @@
 
 package capture
 
-// TODO: Require a function to implement icmp packet captures.
+import (
+	"fmt"
+	"net"
+)
+
+// TODO: Require a function to implement icmp packet captures. Following code won't work in windows
+func OpenICMPCapture() (net.PacketConn, error) {
+	icmpConn, err := net.ListenPacket("ip4:icmp", "0.0.0.0")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open ICMP socket: %w", err)
+	}
+	return icmpConn, nil
+}
--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/KaganAsl/go-traceroute/capture"
+	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
 )
+
+var ttl int = 1
+var packetCount int = 3
+var maxTtl = 30
+var timeout = 1
 
 // type lookup struct {
 // 	name    string
@@ -42,26 +49,58 @@ func main() {
 	defer icmpConn.Close()
 
 	pconn := ipv4.NewPacketConn(conn)
-	if err := pconn.SetTTL(1); err != nil {
-		panic(err)
-	}
-	fmt.Println("Set ttl.")
 
-	msg := []byte("Hello")
-	if _, err := pconn.WriteTo(msg, nil, addr); err != nil {
-		panic(err)
-	}
-	fmt.Println("Msg write.")
+	for range maxTtl {
+		times := make([]float64, packetCount)
+		source := ""
+		var code int
+		if err := pconn.SetTTL(ttl); err != nil {
+			panic(err)
+		}
+		for i := range packetCount {
+			msg := []byte("Hello")
+			startTime := time.Now()
+			if _, err := pconn.WriteTo(msg, nil, addr); err != nil {
+				panic(err)
+			}
 
-	res := make([]byte, 1500)
-	n, src, err := icmpConn.ReadFrom(res)
-	if err != nil {
-		panic(err)
+			res := make([]byte, 1500)
+			icmpConn.SetReadDeadline(time.Now().Add(time.Duration(timeout) * time.Second))
+			n, src, err := icmpConn.ReadFrom(res)
+			if err, ok := err.(net.Error); ok && err.Timeout() {
+				times[i] = -1
+				continue
+			}
+			endTime := time.Since(startTime)
+			endTimeMs := float64(endTime.Microseconds()) / 1000
+			times[i] = endTimeMs
+			if err != nil {
+				panic(err)
+			}
+			res = res[:n]
+			parsedMsg, err := icmp.ParseMessage(1, res)
+			if err != nil {
+				panic(err)
+			}
+			code = parsedMsg.Code
+			if src != nil {
+				source = src.String()
+			}
+		}
+		result := source
+		for _, t := range times {
+			if t <= 0 {
+				result += " *"
+			} else {
+				result += fmt.Sprintf(" %.3fms", t)
+			}
+		}
+		fmt.Println(result)
+		if code == 3 {
+			return
+		}
+		ttl += 1
 	}
-	res = res[:n]
-	fmt.Println("Msg read:")
-	fmt.Println(string(res))
-	fmt.Println(src.String())
 }
 
 func dnsLookUp(domain string) ([]net.IP, error) {
